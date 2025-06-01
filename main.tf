7e5dd649ad8a821af44ff417196ad0d572c63df2
hchapid <hchapid@bancodebogota.com.co>
2025-06-01 18:06:47 -0500

    add rds

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+##############################
+# BUCKET S3
+
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = var.bucket_name
   force_destroy = true
@@ -14,7 +17,7 @@ resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
     suffix = "index.html"
   }
   error_document {
-    key = "error.html"
+    key = "index.html"
   }
 }
 
@@ -25,18 +28,122 @@ resource "aws_s3_bucket_ownership_controls" "frontend_bucket_acl" {
   }
 }
 
-/*resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
+resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = aws_s3_bucket.frontend_bucket.id
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "PublicReadGetObject"
-        Effect = "Allow"
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
         Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.frontend_bucket.arn}/*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
       }
     ]
   })
-}*/
+}
+
+# SSH KEY PAIR
+##############################
+
+resource "aws_key_pair" "kata_key" {
+  key_name   = "key-kata-25"
+  public_key = file("~/.ssh/key-kata-25.pub")
+}
+
+##############################
+# SECURITY GROUP - EC2
+
+resource "aws_security_group" "kata_sg" {
+  name        = "kata-sg"
+  description = "Permitir SSH y HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "port 5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##############################
+# EC2 INSTANCE - BACKEND
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "kata_backend" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.kata_key.key_name
+  vpc_security_group_ids      = [aws_security_group.kata_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "kata-backend-instance"
+  }
+}
+
+###########################
+## INSTANCE RDS
+
+resource "aws_db_instance" "default" {
+  identifier         = "db-postgres"
+  engine             = "postgres"
+  instance_class     = "db.t3.micro"
+  allocated_storage  = 20
+  username           = "hernan"
+  password           = "hchapid53"
+  skip_final_snapshot = true
+  publicly_accessible = true
+
+  tags = {
+    Name = "SimplePostgresRDS"
+  }
+}
